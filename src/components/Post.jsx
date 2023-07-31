2d9846ae2a05173c01f54f694639f13b3b600521
Leandro Ferreira de Oliveira Simeão <leandro.simeao@direcional.com.br>
2023-07-31 18:59:07 -0300

    Commit

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,11 @@
 import styles from './Post.module.css';
 
 export function Post() {
-    return(
+    return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src='https://github.com/LeandroFSimeao.png'/>
+                    <img className={styles.avatar} src='https://github.com/LeandroFSimeao.png' />
                     <div className={styles.authorInfo}>
                         <strong>
                             Leandro Simeão
@@ -19,14 +19,28 @@ export function Post() {
             </header>
 
             <div className={styles.content}>
-            <p>Fala galeraa 👋</p>
+                <p>Fala galeraa 👋</p>
 
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
-           <p> <a href=''>👉 jane.design/doctorcare</a></p>
+                <p>👉{' '}<a href=''>jane.design/doctorcare</a></p>
 
-           <p> <a href=''>#novoprojeto #nlw #rocketseat</a></p>
+                <p>
+                    <a href=''>#novoprojeto </a> {' '}
+                    <a href=''>#nlw </a> {' '}
+                    <a href=''>#rocketseat</a>
+                </p>
             </div>
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea 
+                    placeholder='Deixe um comentário'
+                />
+                <footer>
+                <button type='submit'>Publicar</button>
+
+                </footer>
+            </form>
         </article>
     )
 }
